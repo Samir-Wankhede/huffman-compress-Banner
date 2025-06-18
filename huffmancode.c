3e76fdc4e098c01543016cc8f070bece394c9821
Samir Wankhede <samirsw04@gmail.com>
2025-06-18 22:08:50 +0530

    added heap logic

--- huffmancode.c
+++ huffmancode.c
@@ -1,5 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX_NODES = 256
+
+typedef struct Node {
+    size_t frequency;
+    unsigned char byte;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+typedef struct minHeap {
+    Node *arr;
+    size_t size;
+    size_t capacity;
+} minHeap;
+
+minHeap* create_heap(size_t capacity, Node *leaves){
+    minHeap *h = (minHeap*)malloc(sizeof(minHeap));
+    if (h == NULL){
+        printf("Memory error");
+        return NULL;
+    }
+    h->size = 0;
+    h->capacity = capacity;
+    h->arr = (Node*)malloc(sizeof(Node) * capacity);
+    if (h->arr == NULL) {
+        printf("Memory error");
+        return NULL;
+    }
+    size_t i;
+    for(i=0; i<capacity; i++){
+        h->arr[i] = leaves[i];
+    }
+    h->size = i;
+    i = (h->size / 2) - 1;
+    while(i>=0){
+        heapify(h, i);
+        i--;
+    }
+    return h;
+}
+
+void swap(Node* a, Node* b) {
+    Node temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+void heapify(minHeap* h, size_t i){
+    size_t l = 2 * i + 1;
+    size_t r = 2 * i + 2;
+    size_t min = i;
+    if (l < h->size && h->arr[min].frequency > h->arr[l].frequency){
+        min = l;
+    }
+    if (r < h->size && h->arr[min].frequency > h->arr[r].frequency){
+        min = r;
+    }
+    if (min != i){
+        swap(&h->arr[i], &h->arr[min]);
+        heapify(h, min);
+    }
+}
+
+Node* extractMin(minHeap* h)
+{
+    Node* deleteItem;
+    if (h->size == 0) {
+        printf("\nHeap id empty.");
+        return NULL;
+    }
+    deleteItem = &h->arr[0];
+    h->arr[0] = h->arr[h->size - 1];
+    h->size--;
+    heapify(h, 0);
+    return deleteItem;
+}
+
+void insertHelper(minHeap* h, size_t idx){
+    size_t parent = (idx-1)/2;
+    if (h->arr[parent].frequency > h->arr[idx].frequency){
+        swap(&h->arr[parent], &h->arr[idx]);
+        insertHelper(h, parent);
+    }
+}
+
+void insertHeap(minHeap* h, Node node){
+    if (h->size < h->capacity) {
+        h->arr[h->size] = node;
+        insertHelper(h, h->size);
+        h->size++;
+    }
+}
 
 int main() {
     FILE *fptr = fopen("rickroll.mp4", "rb");
